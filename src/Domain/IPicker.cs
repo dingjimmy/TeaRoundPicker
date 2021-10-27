a9fe5cf67937bb8d4d572857d81868bcdcd7b543
James Dingle <ding_jimmy@hotmail.com>
2021-10-28 00:49:36 +0100

    Missing summary comment

--- src/Domain/IPicker.cs
+++ src/Domain/IPicker.cs
@@ -1,6 +1,8 @@
-﻿
-namespace Domain;
+﻿namespace Domain;
 
+/// <summary>
+/// Represents the logic for picking a participant.
+/// </summary>
 public interface IPicker
 {
     Participant PickParticipant(IEnumerable<Participant> participants);
