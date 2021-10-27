ff084fb8bb10fa7cb8bde625365ba7b5921d1135
James Dingle <ding_jimmy@hotmail.com>
2021-10-28 00:44:15 +0100

    Participant value object

---
+++ src/Domain/Participant.cs
@@ -0,0 +1,27 @@
+﻿namespace domain;
+
+/// <summary>
+/// Represents an individual team member, who is participating in the tea round.
+/// </summary>
+public class Participant
+{
+    /// <summary>
+    /// Gets the name of the participant.
+    /// </summary>
+    public string Name { get; }
+ 
+    /// <summary>
+    /// Creates a new instance of the <see cref="Participant"/> class.
+    /// </summary>
+    /// <param name="name">The name of the new participant.</param>
+    /// <exception cref="ArgumentException"></exception>
+    public Participant(string name)
+    {
+        if (string.IsNullOrWhiteSpace(name))
+        {
+            throw new ArgumentException($"'{nameof(name)}' cannot be null or whitespace.", nameof(name));
+        }
+
+        Name = name;
+    }
+}
