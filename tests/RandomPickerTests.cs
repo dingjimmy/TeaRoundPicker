afd699b1ad774b55c1dccb09e910b58c533fbcae
James Dingle <ding_jimmy@hotmail.com>
2021-10-29 08:02:27 +0100

    Add service that picks participants at random

---
+++ tests/RandomPickerTests.cs
@@ -0,0 +1,27 @@
+using Domain;
+using Xunit;
+
+namespace tests;
+
+public class RandomPickerTests
+{
+    [Fact]
+    public void Pick_a_participant()
+    {
+        // arrange
+        var participants = new[]
+        {
+            new Participant("Fred"),
+            new Participant("Barney"),
+            new Participant("Wilma"),
+            new Participant("Betty")
+        };
+        var picker = new RandomPicker();
+
+        // act
+        var pickedParticipant = picker.PickParticipant(participants);
+
+        // assert that the picked participant is from the original list of participants
+        Assert.Contains(pickedParticipant, participants);
+    }
+}
