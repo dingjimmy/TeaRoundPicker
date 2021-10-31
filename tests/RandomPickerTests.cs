b618ca275b96d4b1c52ac8e55bbb48ccea8ce77e
James Dingle <ding_jimmy@hotmail.com>
2021-10-31 22:17:32 +0000

    Participants can only be entered once; no duplicates.

--- tests/RandomPickerTests.cs
+++ tests/RandomPickerTests.cs
@@ -8,7 +8,7 @@ public class RandomPickerTests
     [Fact]
     public void Pick_one_person_from_a_list_of_participants()
     {
-        // arrange
+        // arrange participants list
         var participants = new[]
         {
             new Participant("Fred"),
@@ -23,5 +23,22 @@ public class RandomPickerTests
 
         // assert that the picked participant is from the original list of participants
         Assert.Contains(pickedParticipant, participants);
+    }
+
+    [Fact]
+    public void Duplicate_participants_are_invalid()
+    {
+        // arrange participants list to contain duplicates
+        var participants = new[]
+        {
+            new Participant("Fred"),
+            new Participant("Barney"),
+            new Participant("Fred"),
+            new Participant("Betty")
+        };
+        var picker = new RandomPicker();
+
+        // assert that a 'duplicate participant' exception is thrown
+        Assert.Throws<DuplicateParticipantsException>(() => picker.PickParticipant(participants));
     }
 }
