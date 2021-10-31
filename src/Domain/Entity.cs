6b3a0bba8fc7f3a369f2024ffe350426ec12f99c
James Dingle <ding_jimmy@hotmail.com>
2021-10-31 18:11:20 +0000

    Participants are entities.

--- src/Domain/Entity.cs
+++ src/Domain/Entity.cs
@@ -5,14 +5,24 @@
 /// </summary>
 public abstract class Entity<T>
 {
+    /// <summary>
+    /// Gets a value which uniquley identifies an entity.
+    /// </summary>
     public virtual T Id { get; protected set; }
 
+    /// <summary>
+    /// Creates a new instance of an entity object.
+    /// </summary>
+    /// <param name="id"></param>
     protected Entity(T id)
     {
         Id = id;
     }
 
-    public override bool Equals(object obj)
+    /// <summary>
+    /// Determines whether the specified entity is equal to the current entity.
+    /// </summary>
+    public override bool Equals(object? obj)
     {
         if (obj is not Entity<T> other)
             return false;
@@ -26,6 +36,9 @@ public abstract class Entity<T>
         return Id.Equals(other.Id);
     }
 
+    /// <summary>
+    /// Determines if two entitiy objects are representing the same entity.
+    /// </summary>
     public static bool operator ==(Entity<T> a, Entity<T> b)
     {
         if (a is null && b is null)
@@ -37,11 +50,17 @@ public abstract class Entity<T>
         return a.Equals(b);
     }
 
+    /// <summary>
+    /// Determines if two entitiy objects are representing different entities.
+    /// </summary>
     public static bool operator !=(Entity<T> a, Entity<T> b)
     {
         return !(a == b);
     }
 
+    /// <summary>
+    /// Returns the hash-code of the current entity.
+    /// </summary>
     public override int GetHashCode()
     {
         return Id.GetHashCode();
