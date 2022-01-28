444bf06f748b48da7b10f13a4cf29114adef98c9
James Dingle <jamesd@amdigital.co.uk>
2022-01-28 13:33:21 +0000

    Add web-api service. Start scaffolding out rest api actions & resources

---
+++ TeaRoundPicker.Service/Controllers/TeaRoundPickerController.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+using TeaRoundPicker.Domain;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace TeaRoundPicker.Service.Controllers
+{
+    [Route("api/session")]
+    [ApiController]
+    public class TeaRoundPickerController : ControllerBase
+    {
+        private readonly List<Session> _Sessions;
+
+        /// <summary>
+        /// Returns a list of all sessions. 
+        /// </summary>
+        [HttpGet]
+        public IEnumerable<Session> Get()
+        {
+            return new Session[] 
+            { 
+                new Session("foo"), new Session("bar"), new Session("fred"), new Session("dave")
+            };
+        }
+
+
+        /// <summary>
+        /// Create new tea round picker session
+        /// </summary>
+        /// <param name=""></param>
+        /// <returns>A new, empty session</returns>
+        [HttpPost]
+        public Session Post([FromBody] NewSessionRequest request)
+        {
+            return new Session(request.Title);
+        }
+
+        // GET api/<ParticipantsController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // PUT api/<ParticipantsController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<ParticipantsController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+
+    public class NewSessionRequest
+    {
+        [Required]
+        [StringLength(256)]
+        public string Title { get; set; }
+
+        public NewSessionRequest(string title)
+        {
+            Title = title;
+        }
+    }
+}
+
+
+
+/* 
+ * # Get a list of all sessions
+ * /session [GET]
+ * 
+ * # Create new tea round picker session
+ * /session [POST]
+ * 
+ * # Add participant to a session
+ * /session/{id}/participant [POST]
+ * 
+ * # Get participants for session
+ * /session/{id}/participant [GET]
+ * 
+ * # Pick participant and close session (creates a pick resurce)
+ *  /session/{id}/pick [POST]
+ *  
---
+++ src/Domain/Session.cs
@@ -0,0 +1,24 @@
+﻿namespace TeaRoundPicker.Domain
+{
+    public class Session
+    {
+        public Guid Id { get; set; }
+
+        public string Title { get; set; } = string.Empty;
+
+        public List<Participant> Participants { get; set; } = new List<Participant> { };
+
+        public Participant? PickedParticipant { get; set; }
+
+        public Session(string title)
+        {
+            if (string.IsNullOrWhiteSpace(title))
+            {
+                throw new ArgumentException($"'{nameof(title)}' cannot be null or whitespace.", nameof(title));
+            }
+
+            Id = Guid.NewGuid();
+            Title = title;
+        }
+    }
+}
